93ee1efddd15628450eb70362d9cc7b4d28d2e22
Apprentice <apprentice@dbc-chicago-009.local>
2013-06-05 18:53:04 -0500

    taking out old authentication code

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,31 +2,28 @@
 class User < ActiveRecord::Base
   include BCrypt
 
-  def self.authenticate(email, password)  
-    if User.find_by_email(email).nil?
-      return nil
-    else  
-      user = User.find_by_email(email) 
-    end 
 
-    if user.password == password
-      return user
-    else
-      return nil
-    end
+
+  def create
   end
 
-  # def password
-  #   @password ||= Password.new(password_hash)
-  # end
-
-  # def password=(new_password)
-  #   @password = Password.create(new_password)
-  #   self.password_hash = @password
-  # end
 
 
 end
 
-# if email and password correspond to a valid user, return that user
-    # otherwise, return nil
+
+
+  # Non-encrypted authentication:
+  # def self.authenticate(email, password)  
+  #   if User.find_by_email(email).nil?
+  #     return nil
+  #   else  
+  #     user = User.find_by_email(email) 
+  #   end 
+
+  #   if user.password == password
+  #     return user
+  #   else
+  #     return nil
+  #   end
+  # end
